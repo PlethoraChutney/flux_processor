fd7241d0c95aca02c81012620d78247088becf85
Rich <rich@posert.com>
2022-04-28 19:01:00 -0700

    Discontinuity marked in graph

--- flux_plotter.R
+++ flux_plotter.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(ggokabeito)
 
-raw_data <- read_csv('processed_test_data.csv', col_types = 'fffdfd')
+raw_data <- read_csv('processed_test_data.csv', col_types = 'ffddff')
 
 data <- raw_data %>% 
   filter(Plate == 'Equilibration') %>% 
@@ -10,17 +10,24 @@ data <- raw_data %>%
   right_join(raw_data, by = c('Row', 'Column')) %>% 
   mutate(Fluorescence = Fluorescence / Equil_Flr)
 
+
 processed <- data %>% 
-  group_by(Condition, Time) %>% 
-  summarize(Mean_Fluorescence = mean(Fluorescence), SD = sd(Fluorescence))
-  # mutate(Low = Mean_Fluorescence - SD, High = Mean_Fluorescence + SD)
+  group_by(Condition, Time, Plate) %>% 
+  summarize(Mean_Fluorescence = mean(Fluorescence))
 
 plates <- data %>% 
   group_by(Plate) %>% 
   summarize(start = min(Time), end = max(Time))
 
+joiners <- processed %>% 
+  group_by(Plate, Condition) %>% 
+  filter(Time == max(Time) | Time == min(Time)) %>% 
+  group_by(Condition) %>% 
+  mutate(Plate = lead(Plate)) %>% 
+  drop_na()
+
 processed %>% 
-  ggplot(aes(x = Time, y = Mean_Fluorescence, color = Condition)) +
+  ggplot(aes(x = Time, y = Mean_Fluorescence, color = Condition, group = interaction(Condition, Plate))) +
   theme_minimal() +
   geom_rect(
     inherit.aes = FALSE,
@@ -30,8 +37,16 @@ processed %>%
     ymax = Inf,
     alpha = 0.05
   ) +
-  geom_line(data = data, aes(y = Fluorescence, group = interaction(Row, Column)), alpha = 0.2) +
-  geom_line(size = 2) +
+  geom_line(data = data, aes(y = Fluorescence, group = interaction(Row, Column, Plate)), alpha = 0.2) +
+  geom_line(
+    data = joiners,
+    aes(x = Time, y = Mean_Fluorescence, group = interaction(Condition, Plate), color = Condition),
+    inherit.aes = FALSE,
+    size = 2,
+    linetype = 'dotted',
+    lineend = 'round'
+  ) +
+  geom_line(size = 2, lineend = 'round') +
   scale_color_okabe_ito() +
   scale_fill_manual(values = c(
     "#0077bb",
