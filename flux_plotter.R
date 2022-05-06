999b14db0320f4a921e6215b0b5d74ead9e1e020
Rich <rich@posert.com>
2022-05-06 14:54:00 -0700

    Auto-detect file

--- flux_plotter.R
+++ flux_plotter.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(ggokabeito)
 
-raw_data <- read_csv('processed_Flux Assay Test.csv', col_types = 'ffddff')
+raw_data <- read_csv(list.files(pattern = 'processed.*.csv')[1], col_types = 'dffffd')
 
 data <- raw_data %>% 
   filter(Plate == 'Equilibration') %>% 
@@ -13,7 +13,8 @@ data <- raw_data %>%
 
 processed <- data %>% 
   group_by(Condition, Time, Plate) %>% 
-  summarize(Mean_Fluorescence = mean(Fluorescence))
+  summarize(Mean_Fluorescence = mean(Fluorescence), SD = sd(Fluorescence)) %>% 
+  mutate(Low = Mean_Fluorescence - SD, High = Mean_Fluorescence + SD)
 
 plates <- data %>% 
   group_by(Plate) %>% 
@@ -37,7 +38,6 @@ processed %>%
     ymax = Inf,
     alpha = 0.05
   ) +
-  geom_line(data = data, aes(y = Fluorescence, group = interaction(Row, Column, Plate)), alpha = 0.2) +
   geom_line(
     data = joiners,
     aes(x = Time, y = Mean_Fluorescence, group = interaction(Condition, Plate), color = Condition),
@@ -54,10 +54,45 @@ processed %>%
     "#cc3311",
     "#ee3377"
   )) +
+  scale_y_continuous(
+    breaks = seq(0, 1, 0.25)
+  ) +
   labs(
     y = 'Relative Fluorescence',
     x = 'Time (s)',
     color = 'Condition',
     fill = 'Reagent added'
-  )
-ggsave('flux_plot.png', width = 6, height = 4)
+  ) +
+  expand_limits(y = c(0, 1))
+ggsave('_Mean.png', width = 6, height = 4)
+
+data %>% 
+  ggplot(aes(x = Time, y = Fluorescence, color = Condition, group = interaction(Row, Column, Plate))) +
+  theme_minimal() +
+  geom_rect(
+    inherit.aes = FALSE,
+    data = plates,
+    aes(xmin = start, xmax = end, fill = Plate),
+    ymin = -Inf,
+    ymax = Inf,
+    alpha = 0.05
+  ) +
+  geom_line(lineend = 'round') +
+  scale_color_okabe_ito() +
+  scale_fill_manual(values = c(
+    "#0077bb",
+    "#009988",
+    "#cc3311",
+    "#ee3377"
+  )) +
+  scale_y_continuous(
+    breaks = seq(0, 1, 0.25)
+  ) +
+  labs(
+    y = 'Relative Fluorescence',
+    x = 'Time (s)',
+    color = 'Condition',
+    fill = 'Reagent added'
+  ) +
+  expand_limits(y = c(0, 1))
+ggsave('_Raw.png', width = 6, height = 4)
--- plate-processor.py
+++ plate-processor.py
@@ -1,56 +1,55 @@
-from string import ascii_uppercase
+import re
 import os
 import argparse
 import pandas as pd
 
+well_pattern = re.compile('[A-Z][0-9]{2}')
 
-def read_plate_csv(filename, sampling_rate, conditions_key, plate_swap):
+def read_plate_csv(filename, conditions_key, plate_swap):
     with open(filename, 'r') as f:
         # un-read cells are filled with '-'
         #
         # separate plate reads are separated by a line with no entries in it
         # with our processing this becomes [''], which we'll replace with 'end_plate'
-        lines = [[y.strip() for y in x.split(',')] for x in f]
-        lines = [[int(y) for y in x if y != '-'] if x != [''] else 'end_plate' for x in lines]
+        lines = [[y.strip() for y in x.split(':')] for x in f]
 
-    row = 0
-    time_index = 0
-    plate_reads = []
+    reads = []
+    current_time = 0
+    current_cycle = 0
     for line in lines:
-        if line == 'end_plate':
-            # reset every time we end a plate
-            row = 0
-            time_index += 1
-            continue
-        
-        time = time_index * sampling_rate
-        offset = 0
-        plate = 'Equilibration'
-        # swap is [removal_time, removal_duration, condition]
-        for swap in plate_swap:
-            if swap[0] * sampling_rate <= time:
-                offset += swap[1]
-                plate = swap[2]
-            else:
-                break
-        time += offset
+        try:
+            if line[1] == '-':
+                continue
+        except IndexError:
+            pass
+        if re.search(well_pattern, line[0]):
+            row = line[0][0]
+            column = int(line[0][1:])
+            flr = float(line[1])
+            sample = conditions_key[row][column-1]
+            plate_condition = 'Equilibration'
 
-        for col_index in range(len(line)):
-            # add an entry to our eventual dataframe
-            # 
-            # this is faster than directly modifying a pd dataframe, apparently
+            time = current_time
+            for plate in plate_swap:
+                if current_cycle > plate[0]:
+                    time += plate[1]
+                    plate_condition = plate[2]
 
-            plate_reads.append({
-                'Row': ascii_uppercase[row],
-                'Column': col_index + 1,
+            reads.append({
                 'Time': time,
-                'Fluorescence': line[col_index],
-                'Condition': conditions_key[col_index][row],
-                'Plate': plate
+                'Plate': plate_condition,
+                'Row': row,
+                'Column': column,
+                'Condition': sample,
+                'Fluorescence': flr
             })
-        row += 1
-    
-    return pd.DataFrame(plate_reads)
+        elif line[0] == 'Time [s]':
+            current_time = int(line[1])
+        elif line[0] == 'Cycle':
+            current_cycle = int(line[1])
+
+
+    return pd.DataFrame(reads)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -68,11 +67,6 @@ def main():
         help = 'Filename for conditions key csv.'
     )
     parser.add_argument(
-        'sampling_rate',
-        type = int,
-        help = 'Sampling rate for plates, in seconds. Default 4'
-    )
-    parser.add_argument(
         '-p',
         '--plate-swap',
         type = str,
@@ -87,9 +81,15 @@ def main():
         plate_swap = [(int(x[0]), int(x[1]), x[2].replace('_', ' ')) for x in args.plate_swap]
     else:
         plate_swap = []
-    conditions_key = pd.read_csv(args.conditions_key, header = None)
 
-    data = read_plate_csv(args.plate_data, args.sampling_rate, conditions_key, plate_swap)
+    with open(args.conditions_key, 'r') as f:
+        conditions_list = [x.rstrip().split(',') for x in f]
+
+    conditions_key = {}
+    for row in conditions_list[1:]:
+        conditions_key[row[0]] = row[1:]
+
+    data = read_plate_csv(args.plate_data, conditions_key, plate_swap)
     output_name = f"processed_{os.path.split(args.plate_data)[1]}"
     data.to_csv(output_name, index = False)
 
