84ca1cef18199b5e6c25601fc6f1ae2dd2a20b69
Rich <rich@posert.com>
2022-05-06 18:46:19 -0700

    Comments

--- plate-processor.py
+++ plate-processor.py
@@ -8,10 +8,7 @@ well_pattern = re.compile('[A-Z][0-9]{2}')
 
 def read_plate_csv(filename, conditions_key, plate_swap):
     with open(filename, 'r') as f:
-        # un-read cells are filled with '-'
-        #
-        # separate plate reads are separated by a line with no entries in it
-        # with our processing this becomes [''], which we'll replace with 'end_plate'
+        # Most lines are either Time: time or Well: Flr
         lines = [[y.strip() for y in x.split(':')] for x in f]
 
     reads = []
@@ -19,23 +16,41 @@ def read_plate_csv(filename, conditions_key, plate_swap):
     current_cycle = 0
     for line in lines:
         try:
+            # Unread wells are still in the file, with a fluorescence
+            # value of '-'
             if line[1] == '-':
                 continue
         except IndexError:
             pass
-        if re.search(well_pattern, line[0]):
-            row = line[0][0]
-            column = int(line[0][1:])
-            flr = float(line[1])
-            sample = conditions_key[row][column-1]
-            plate_condition = 'Equilibration'
 
+        if re.search(well_pattern, line[0]):
+            # rows are always one letter
+            row = line[0][0]
+
+            # remaining characters are the column
+            column = int(line[0][1:])
+
+            # we separated on the colon, so FLR is stored in the second element
+            flr = float(line[1])
+
+            # This `sample` is "What's in the well"
+            sample = conditions_key[row][column-1]
+
+            # start out with the plate reader time and equilibration plate
+            plate_condition = 'Equilibration'
             time = current_time
             for plate in plate_swap:
+                # then if we find a plate swap that occurred before this
+                # cycle
                 if current_cycle > plate[0]:
+                    # add the duration of the plate swap to our time
                     time += plate[1]
+                    # and update our current plate condition
                     plate_condition = plate[2]
 
+            # list of dicts is faster than appending to a pd dataframe
+            # or at least it used to be:
+            # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.append.html
             reads.append({
                 'Time': time,
                 'Plate': plate_condition,
@@ -44,8 +59,16 @@ def read_plate_csv(filename, conditions_key, plate_swap):
                 'Condition': sample,
                 'Fluorescence': flr
             })
+        # if it's a time line, update our pre-plate-swap time
         elif line[0] == 'Time [s]':
             current_time = int(line[1])
+
+        # if it's a new cycle, update our cycle
+        #
+        # technically, we only read time once per cycle, so
+        # these could be combined. But we read the line anyway,
+        # so it doesn't cost much to make sure we're staying in sync
+        # with the plate reader.
         elif line[0] == 'Cycle':
             current_cycle = int(line[1])
 
@@ -79,15 +102,20 @@ def main():
     args = parser.parse_args()
 
     if args.plate_swap:
+        # (cycle, duration, plate condition)
         plate_swap = [(int(x[0]), int(x[1]), x[2].replace('_', ' ')) for x in args.plate_swap]
     else:
         plate_swap = []
 
     with open(args.conditions_key, 'r') as f:
+        # conditions list is a list of the rows of our csv
         conditions_list = [x.rstrip().split(',') for x in f]
 
     conditions_key = {}
+    # skip the first row, which is just column numbers
     for row in conditions_list[1:]:
+        # first element is the row letter, which will be our key
+        # the remaining elements are the samples.
         conditions_key[row[0]] = row[1:]
 
     data = read_plate_csv(args.plate_data, conditions_key, plate_swap)
